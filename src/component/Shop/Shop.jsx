2484b4df033f3172ff7478af1f88e0af77341c33
mehedi-hassan-sohan <mehedihassansohan80@gmail.com>
2023-03-28 02:57:34 +0600

    local storage setup progresce

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'; 
+import { addToDb, getShoppingCart } from '../../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
@@ -11,11 +12,22 @@ const Shop = () => {
     fetch('products.json')
       .then(res => res.json())
       .then(data => setProducts(data));
-  }, []);
+  }, []); 
+
+  useEffect(()=>{
+    const storedCart = getShoppingCart()
+    for(const id in storedCart){
+      const addedProduct =products.find(product => product.id ===id)
+      const quantity = setCart[id]
+      addedProduct.quantity = quantity
+
+    }
+  },[products])
 
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id)
   };
 
   return (
